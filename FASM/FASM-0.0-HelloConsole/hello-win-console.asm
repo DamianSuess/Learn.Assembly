fc002e526a86b2082d8e771a04bf22077dffe4a7
Damian Suess <djsuess@gmail.com>
2025-11-11 13:48:48 -0500

    updated CLI example

--- FASM/FASM-0.0-HelloConsole/hello-win-console.asm
+++ FASM/FASM-0.0-HelloConsole/hello-win-console.asm
@@ -8,9 +8,13 @@ include 'win32a.inc'
 section '.text' code readable executable
 start:
   push _welcome
+  push _msgFormat
   call [printf]
   pop ecx
 
+  ; Pause and wait for input
+  push _wait
+  call [printf]
   call [scanf]
 
   push 0
@@ -18,10 +22,19 @@ start:
 
 section '.rdata' data readable writeable
 
-_welcome db 'Hello, my good chum!', 10, 0
+  ; Remember to include char '10' for line feed
+  _msgFormat  db 'My message: %s', 0
+  _welcome    db 'Hello, my good chum!', 10, 0
+  _wait       db 'Press enter to continue...', 0
 
 section '.idata' data readable import
-  library kernel32, 'kernel32.dll', \
-          msvcrt,   'msvcrt.dll'
-  import kernel32, ExitProcess, 'ExitProcess'
-  import msvcrt, printf, 'printf', scanf, 'scanf'
+  library
+    kernel32, 'kernel32.dll', \
+    msvcrt,   'msvcrt.dll'
+
+  import kernel32,
+         ExitProcess, 'ExitProcess'
+
+  import msvcrt,
+         printf, 'printf',
+         scanf, 'scanf'
--- FASM/FASM-0.0-HelloConsole/hello-win-msgbox.asm
+++ FASM/FASM-0.0-HelloConsole/hello-win-msgbox.asm
@@ -1,6 +1,6 @@
 ; Hello World - Windows MessageBox
 
-format PE console       ; Windows Portable Exectuable format
+format PE gui       ; Windows Portable Exectuable format
 entry start             ; Entry point of app
 
 include 'win32a.inc'    ; Windows API
@@ -8,18 +8,35 @@ include 'win32a.inc'    ; Windows API
 ; Data section
 section '.data' readable writable
   caption db 'hello', 0           ; Caption of MsgBox
-  message db 'Hello, World!', 0   ; Message to display
+  message1 db 'Hello, World 1', 0   ; Message to display
+  message2 db 'Hello, World 2', 0   ; Message to display
+  message3 db 'Hello, World 3', 0   ; Message to display
 
 ; Code Section
 section '.code' executable
 start:
-  invoke MessageBox, NULL, message, caption, MB_OK    ; Call MessageBox function
+
+  ; See section 3.1.3 of the FASM guide
+  ; Call procedure through pointer labelled by the 1st argument
+  invoke MessageBox, NULL, message1, caption, MB_OK    ; Call MessageBox function
+
+  ; Calls directly to the procuedure specified
+  stdcall [MessageBox], 0, message2, caption, MB_OK
+
+  push MB_OK
+  push caption
+  push message3
+  push 0
+  call [MessageBox]
+
   invoke ExitProcess, NULL                            ; Exit process
 
+; See section 3.1.2 Imports of the FASM guide
 section '.idata' import data readable writeable
-;data import
-  library kernel,'KERNEL32.DLL',\
-          user,'USER32.DLL'
+;data import                      ; <--- alternative to "secction '.idata' import above
+  library \
+    kernel,'KERNEL32.DLL',\
+    user,'USER32.DLL'
 
   import kernel,\
     ExitProcess,'ExitProcess'
